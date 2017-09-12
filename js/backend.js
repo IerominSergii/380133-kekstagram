be16f79e3f865f8f0c3ee15fc8ee12793d411224
IerominSergii <iconboooook@gmail.com>
2017-09-12 22:12:49 +0300

    Создай модуль backend.js С функциями (load и save)

---
+++ js/backend.js
@@ -0,0 +1,48 @@
+// Файл backend.js
+'use strict';
+
+(function () {
+  var SERVER_URL = 'https://1510.dump.academy/kekstagram';
+
+  var setup = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      try {
+        onLoad(xhr.responseText);
+      } catch (err) {
+        onError(err.message);
+      }
+    });
+
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;// 10s
+
+    return xhr;
+  };
+
+  window.backend = {
+    load: function (onLoad, onError) {
+      var xhr = setup(onLoad, onError);
+
+      xhr.open('GET', SERVER_URL + '/data');
+      xhr.send();
+    },
+
+    save: function (data, onLoad, onError) {
+      var xhr = setup(onLoad, onError);
+
+      xhr.open('POST', SERVER_URL);
+      xhr.send(data);
+    },
+  };
+})();
